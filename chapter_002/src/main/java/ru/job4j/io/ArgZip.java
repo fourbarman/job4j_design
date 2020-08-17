e44051e1a914fb7ad0c03d07c894f2c8eff8aac7
fourbarman <Puasson246>
2020-08-17 16:44:22 +0400

    Arhiv beta2

--- chapter_002/src/main/java/ru/job4j/io/ArgZip.java
+++ chapter_002/src/main/java/ru/job4j/io/ArgZip.java
@@ -2,6 +2,14 @@ package ru.job4j.io;
 
 import java.io.File;
 
+/**
+ * ArgZip.
+ * Validate args.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version %I%, %G%.
+ * @since 17.08.2020.
+ */
 public class ArgZip {
     private final String[] args;
 
@@ -10,30 +18,26 @@ public class ArgZip {
     }
 
     public boolean valid() {
-        if (args.length != 3
-            ) {
-            throw new IllegalStateException("Not enough parameters!");
+        if (args.length != 3) {
+            throw new IllegalStateException("Not enough parameters!" +
+                    "\n Example: -d=C:\\projects\\job4j_design\\chapter_001\\ -e=class -o=project.zip");
         }
-        if (!args[0].startsWith("-d=")
-        ) {
-            throw new IllegalStateException("Wrong \"directory\" parameter");
-        }
-        if (!args[1].startsWith("-e=")
-        ) {
-            throw new IllegalStateException("Wrong \"exclude\" parameter");
-        }
-        if (!args[2].startsWith("-o=")
-        ) {
-            throw new IllegalStateException("Wrong \"output\" parameter");
-        }
-        File dir = new File(args[0].split("=")[1]);
-        if (!dir.exists()) {
-            throw new IllegalStateException("Directory " + dir.getAbsolutePath() + " doesn't exist!");
-        }
-        if (!dir.isDirectory())
-        {
-            throw new IllegalStateException("Wrong \" not a directory \" parameter ");
-        }
+//        if (!args[0].startsWith("-d=")) {
+//            throw new IllegalStateException("Wrong \"directory\" parameter");
+//        }
+//        if (!args[1].startsWith("-e=")) {
+//            throw new IllegalStateException("Wrong \"exclude\" parameter");
+//        }
+//        if (!args[2].startsWith("-o=")) {
+//            throw new IllegalStateException("Wrong \"output\" parameter");
+//        }
+//        File dir = new File(args[0].split("=")[1]);
+//        if (!dir.exists()) {
+//            throw new IllegalStateException("Directory " + dir.getAbsolutePath() + " doesn't exist!");
+//        }
+//        if (!dir.isDirectory()) {
+//            throw new IllegalStateException("Wrong \" not a directory \" parameter ");
+//        }
         return true;
     }
 
--- chapter_002/src/main/java/ru/job4j/io/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -8,7 +8,21 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
+/**
+ * Zip.
+ * Zip catalogue.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version %I%, %G%.
+ * @since 17.08.2020.
+ */
 public class Zip {
+    /**
+     * Zips catalogue to target file.
+     *
+     * @param sources List of file paths.
+     * @param target  Target file to zip into.
+     */
     public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.getPath())))) {
             for (Path path : sources) {
@@ -17,29 +31,29 @@ public class Zip {
             try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(target))) {
                 zip.write(out.readAllBytes());
             }
-            System.out.println("Zip success");
+            System.out.println("Successfully zip to: " + target.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-//    public void packSingleFile(File source, File target) {
-//        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-//            zip.putNextEntry(new ZipEntry(source.getPath()));
-//            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-//                zip.write(out.readAllBytes());
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
+    /**
+     * Main method.
+     * Usage with tree args parameters.
+     * -d - directory - catalogue.
+     * -e - exclude - extension to exclude.
+     * -o - output - output filename.
+     *
+     * @param args Args Catalogue, Extension, Output file.
+     * @throws IOException Exception if args are not valid.
+     */
     public static void main(String[] args) throws IOException {
         ArgZip a = new ArgZip(args);
-        SearchFiles searcher = new SearchFiles(p -> !p.toFile().getName().endsWith(a.exclude()));
-        Files.walkFileTree(Paths.get(a.directory()), searcher);
-        String path = Paths.get(a.directory()) + ".zip";
-        File file = new File(path);
-        new Zip().packFiles(searcher.getPaths(), file);
+        if (a.valid()) {
+            SearchFiles searcher = new SearchFiles(p -> !p.toFile().getName().endsWith(a.exclude()));
+            Files.walkFileTree(Paths.get(a.directory()), searcher);
+            String path = a.output();
+            new Zip().packFiles(searcher.getPaths(), new File(path));
+        }
     }
 }
