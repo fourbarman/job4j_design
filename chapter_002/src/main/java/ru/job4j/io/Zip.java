550028c547744a03a50d5bf09e214e8e5badcf82
fourbarman <Puasson246>
2020-08-19 14:54:04 +0400

    Zip catalogue. v.2.

--- chapter_002/src/main/java/ru/job4j/io/Zip.java
+++ chapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -27,14 +27,28 @@ public class Zip {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.getPath())))) {
             for (Path path : sources) {
                 zip.putNextEntry(new ZipEntry(path.toString()));
-            }
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(target))) {
-                zip.write(out.readAllBytes());
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(target))) {
+                    zip.write(out.readAllBytes());
+                }
             }
             System.out.println("Successfully zip to: " + target.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Returns List of paths of files with given extension.
+     *
+     * @param directory Directory path to search in.
+     * @param ext       Given extension.
+     * @return List of paths.
+     * @throws IOException Exception.
+     */
+    public static List<Path> search(Path directory, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> !p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(directory, searcher);
+        return searcher.paths;
     }
 
     /**
@@ -50,10 +64,7 @@ public class Zip {
     public static void main(String[] args) throws IOException {
         ArgZip a = new ArgZip(args);
         if (a.valid()) {
-            SearchFiles searcher = new SearchFiles(p -> !p.toFile().getName().endsWith(a.exclude()));
-            Files.walkFileTree(Paths.get(a.directory()), searcher);
-            String path = a.output();
-            new Zip().packFiles(searcher.getPaths(), new File(path));
+            new Zip().packFiles(search(Paths.get(a.directory()), a.exclude()), new File(a.output()));
         }
     }
 }
