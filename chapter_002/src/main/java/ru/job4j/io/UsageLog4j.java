4bb3e20d3b2c0ae15fd370846a1c0aae00f9e9b4
fourbarman <Puasson246>
2020-10-02 15:36:35 +0400

    Slf4j log primitives.

--- chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,23 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        byte b = 16;
+        short s = 32;
+        int i = 33;
+        long l = 922337203;
+        double d = 244.41234;
+        float f = 145.45F;
+        boolean bool = true;
+        char ch = 'G';
+
+        LOG.debug("byte : {}, " +
+                "short : {}, " +
+                "int : {}, " +
+                "long : {}, " +
+                "double : {}, " +
+                "float : {}, " +
+                "boolean : {}, " +
+                "char : {}", b, s, i, l, d, f, bool, ch);
     }
 }
