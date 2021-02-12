7a59d6dc0b31c2bbf03159c1376eaa9aeea055ec
fourbarman <Puasson246>
2021-02-12 16:43:55 +0400

    Quartz. Job scheduler task. With parameters and database.

--- chapter_003/src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ chapter_003/src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -4,8 +4,9 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -14,63 +15,82 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 /**
  * AlertRabbit.
- * Initialize quartz class and print string with interval from rabbit.properties file.
+ * Initialize quartz class and stores timestamp to DB.
  *
  * @author fourbarman (maks.java@yandex.ru).
  * @version %I%, %G%.
- * @since 08.02.2021.
+ * @since 12.02.2021.
  */
 public class AlertRabbit {
-    /**
-     * Main method.
-     *
-     * @param args Arguments.
-     */
+    static Connection connection;
+
     public static void main(String[] args) {
-        String pathToProp = System.getProperty("user.dir") + "/chapter_003/src/main/java/ru/job4j/quartz/rabbit.properties";
         try {
+            String pathToProp = System.getProperty("user.dir") + "/chapter_003/src/main/java/ru/job4j/quartz/rabbit.properties";
+            AlertRabbit alertRabbit = new AlertRabbit();
+            alertRabbit.init(pathToProp);
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("store", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(getInterval(pathToProp))
+                    .withIntervalInSeconds(5)
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (Exception se) {
             se.printStackTrace();
         }
+
     }
 
     /**
-     * Returns property from file.
-     *
-     * @param path String path to file.
-     * @return Int interval.
+     * Connect to DB and retrieve Connection instance and interval.
      */
-    private static int getInterval(String path) {
-        int interval = 0;
+    public void init(String path) {
         try (InputStream input = new FileInputStream(path)) {
-            Properties prop = new Properties();
-            prop.load(input);
-            interval = Integer.parseInt(prop.getProperty("rabbit.interval"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            Properties config = new Properties();
+            config.load(input);
+            Class.forName(config.getProperty("jdbc.driver"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("jdbc.url"),
+                    config.getProperty("jdbc.username"),
+                    config.getProperty("jdbc.password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
         }
-        return interval;
     }
 
-    /**
-     * Rabbit
-     */
     public static class Rabbit implements Job {
+
+        public Rabbit() {
+            System.out.println(hashCode());
+        }
+
         @Override
-        public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ...");
+        public void execute(JobExecutionContext context) {
+            try {
+                this.saveTime(connection);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        public void saveTime(Connection cnt) throws SQLException {
+            try (PreparedStatement ps = cnt.prepareStatement("INSERT INTO rabbit (created_date) VALUES (?)")) {
+                LocalDateTime timeStamp = LocalDateTime.now();
+                ps.setObject(1, timeStamp);
+                ps.execute();
+            }
         }
     }
-
 }
