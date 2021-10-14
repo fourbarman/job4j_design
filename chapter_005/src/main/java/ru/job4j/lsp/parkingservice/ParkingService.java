012fa34e6a97f7c77be0e4a4b72c19a69a0e146f
fourbarman <Puasson246>
2021-10-14 14:09:22 +0400

    Interfaces for Parking service in LSP.

---
+++ chapter_005/src/main/java/ru/job4j/lsp/parkingservice/ParkingService.java
@@ -0,0 +1,12 @@
+package ru.job4j.lsp.parkingservice;
+/**
+ * ParkingService.
+ * <p>
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version %I%, %G%.
+ * @since 14.10.2021.
+ */
+public interface ParkingService {
+    boolean park(Vehicle vehicle);
+}
---
+++ chapter_005/src/main/java/ru/job4j/lsp/parkingservice/Vehicle.java
@@ -0,0 +1,12 @@
+package ru.job4j.lsp.parkingservice;
+/**
+ * Vehicle.
+ * <p>
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version %I%, %G%.
+ * @since 14.10.2021.
+ */
+public interface Vehicle {
+    int getSize();
+}
