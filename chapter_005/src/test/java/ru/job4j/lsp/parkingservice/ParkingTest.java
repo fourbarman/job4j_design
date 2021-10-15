f724ba6fde12319c74450a1adc9256ce83b2604a
fourbarman <Puasson246>
2021-10-15 15:19:13 +0400

    Parking service. Tests refactored.

--- chapter_005/src/test/java/ru/job4j/lsp/parkingservice/ParkingTest.java
+++ chapter_005/src/test/java/ru/job4j/lsp/parkingservice/ParkingTest.java
@@ -1,18 +1,40 @@
 package ru.job4j.lsp.parkingservice;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
+/**
+ * Test.
+ *
+ * @author fourbarman (maks.java@yandex.ru).
+ * @version %I%, %G%.
+ * @since 15.10.2021.
+ */
 public class ParkingTest {
+    ParkingService parking;
+    List<ParkPlace> parkPlaceList;
+    ParkPlace carParkPlaces;
+    ParkPlace truckParkPlaces;
+
+    @Before
+    public void setVars() {
+        carParkPlaces = new CarParkPlaces(2);
+        truckParkPlaces = new TruckParkPlaces(2);
+        parkPlaceList = List.of(carParkPlaces, truckParkPlaces);
+        parking = new Parking(parkPlaceList);
+    }
+
     /**
      * When add a Car and there is enough places in ParkingService than return true.
      */
     @Test
     public void whenCarAddedAndEnoughParkingPlacesThanReturnTrue() {
-        ParkingService parking = new Parking(20, 10);
-        assertTrue(parking.park(new Car(1)));
+        assertTrue(parking.park(new Car()));
     }
 
     /**
@@ -21,9 +43,8 @@ public class ParkingTest {
      */
     @Test
     public void whenCarAddedAndEnoughParkingPlacesThanCarPlacesIsReducedByOne() {
-        ParkingService parking = new Parking(20, 10);
-        assertThat(CarParkPlaces.getPlacesNumber(), is(19));
-        assertThat(TruckParkPlaces.getPlacesNumber(), is(10));
+        assertThat(carParkPlaces.getPlaceNumber(), is(1));
+        assertThat(truckParkPlaces.getPlaceNumber(), is(2));
     }
 
     /**
@@ -31,8 +52,9 @@ public class ParkingTest {
      */
     @Test
     public void whenCarAddedAndNotEnoughParkingPlacesThanReturnFalse() {
-        ParkingService parking = new Parking(0, 10);
-        assertFalse(parking.park(new Car(1)));
+        parking.park(new Car());
+        parking.park(new Car());
+        assertFalse(parking.park(new Car()));
     }
 
     /**
@@ -40,7 +62,6 @@ public class ParkingTest {
      */
     @Test
     public void whenTruckAddedAndEnoughParkingPlacesThanReturnTrue() {
-        ParkingService parking = new Parking(20, 10);
         assertTrue(parking.park(new Truck(2)));
     }
 
@@ -50,10 +71,9 @@ public class ParkingTest {
      */
     @Test
     public void whenTruckAddedAndEnoughParkingPlacesThanTruckPlacesIsMinusOnePlace() {
-        ParkingService parking = new Parking(20, 10);
         assertTrue(parking.park(new Truck(2)));
-        assertThat(TruckParkPlaces.getPlacesNumber(), is(8));
-        assertThat(CarParkPlaces.getPlacesNumber(), is(20));
+        assertThat(truckParkPlaces.getPlaceNumber(), is(1));
+        assertThat(carParkPlaces.getPlaceNumber(), is(2));
     }
 
     /**
@@ -63,9 +83,8 @@ public class ParkingTest {
      */
     @Test
     public void whenTruckAddedAndNotEnoughParkingPlacesButEnoughInCarParkThanReturnTrue() {
-        ParkingService parking = new Parking(20, 0);
         assertTrue(parking.park(new Truck(2)));
-        assertThat(CarParkPlaces.getPlacesNumber(), is(18));
+        assertThat(carParkPlaces.getPlaceNumber(), is(0));
     }
 
     /**
@@ -73,7 +92,8 @@ public class ParkingTest {
      */
     @Test
     public void whenCarAddedAndEnoughParkingPlacesThanReturnTrue() {
-        ParkingService parking = new Parking(0, 0);
+        parking.park(new Truck(2));
+        parking.park(new Truck(2));
         assertFalse(parking.park(new Truck(2)));
     }
 }
